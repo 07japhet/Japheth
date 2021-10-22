a89bdaabadddcf7c74aa108895f6a1a15d4a0739
07japhet <90698701+07japhet@users.noreply.github.com>
2021-10-22 04:01:09 -0700

    Add files via upload

---
+++ Library management system.c
@@ -0,0 +1,20 @@
+
+/*Project Title:Library Management system
+  Author:  Japhet Makori
+  Compiler:GCC
+  Languages:c99
+  License: MIT
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Library Management System!\n");
+    printf("Add book\n");
+    printf("Search book\n");
+    printf("Edit book\n");
+    printf("Delete book\n");
+    return 0;
+}
